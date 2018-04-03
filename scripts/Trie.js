fb0fba278d77c5e721b8e97e41fa2b1b9f19efb3
Eric Mellow <ericmellow@gmail.com>
2018-04-03 14:12:33 -0600

    Add insert functionality

--- scripts/Trie.js
+++ scripts/Trie.js
@@ -2,11 +2,23 @@ import Node from '../scripts/Node';
 
 export default class Trie {
   constructor() {
-    this.count = 0;
+    this.wordCount = 0;
+    this.root = new Node();
+
   }
 
-  insert(word) {
+  insert(word, currentNode = this.root) {
+    if (!word.length) {
+      this.isAWord = true;
+      return;
+    }
 
+    if (!currentNode.childrenObj[word[0]]) {
+      currentNode.childrenObj[word[0]] = new Node();
+      currentNode.childrenObj[word[0]].value = word[0];
+    }
+    currentNode = currentNode.childrenObj[word[0]]
+    this.insert(word.substr(1), currentNode)
   }
 
   suggest(word) {
@@ -19,5 +31,9 @@ export default class Trie {
 
   delete(word) {
 
+  }
+
+  count() {
+    return this.wordCount;
   }
 }
