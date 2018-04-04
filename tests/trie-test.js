dcc1a5e8e1e90c9c5be624149c76f38658d271ea
Eric Mellow <ericmellow@gmail.com>
2018-04-04 10:44:57 -0600

    Add suggest functionality

--- tests/trie-test.js
+++ tests/trie-test.js
@@ -2,4 +2,18 @@ import { expect } from 'chai';
 import Trie from '../scripts/Trie';
 
 describe('Trie', () => {
+  let trie = new Trie();
+
+  trie.insert('pin');
+  trie.insert('pint');
+  trie.insert('pie');
+  trie.insert('pizza');
+  trie.insert('pine');
+  trie.insert('pot');
+
+  console.log(JSON.stringify(trie, null, 2));
+
+  trie.suggest('pi');
+  console.log(trie.suggest('pi'));
+
 })
